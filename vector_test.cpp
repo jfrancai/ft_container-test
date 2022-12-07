569760fb646e95c5b5bc2edac9c7b6517c1086b6
Julien FRANCAIS <jfrancai@student.42.fr>
2022-12-07 18:36:03 +0100

    first commit: initializing tests file for ft_container

---
+++ vector_test.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+#include <ft/vector.hpp>
+
+/*
+ *
+ * Usefull links :
+ * Reference Testing	: https://google.github.io/googletest/reference/testing.html
+ * Reference Assertions : https://google.github.io/googletest/reference/assertions.html
+ *
+ */
+
+namespace {
+
+	//		To create a test:
+	//
+	// 1)	Use the TEST() macro to define and name a test function.
+	//		These are ordinary C++ functions that don’t return a value.
+	//
+	// 2)	In this function, along with any valid C++ statements you want to include,
+	// 		use the various googletest assertions to check values.
+	//
+	// 3)	The test’s result is determined by the assertions;
+	//		if any assertion in the test fails (either fatally or non-fatally), 
+	//		or if the test crashes, the entire test fails. Otherwise, it succeeds.
+
+	//		To create a fixture:
+	//
+	// 1)	Derive a class from ::testing::Test.
+	// 		Start its body with protected:,
+	// 		as we’ll want to access fixture members from sub-classes.
+	//
+	// 2)	Inside the class, declare any objects you plan to use.
+	//
+	// 3)	If necessary, write a default constructor or SetUp() function to prepare the objects for each test.
+	// 		A common mistake is to spell SetUp() as Setup() with a small u.
+	// 		Use override in C++11 to make sure you spelled it correctly.
+	//
+	// 4)	If necessary, write a destructor or TearDown() function to release any resources you allocated in SetUp().
+	// 		To learn when you should use the constructor/destructor and when you should use SetUp()/TearDown(), read the FAQ.
+	//
+	// 5)	If needed, define subroutines for your tests to share.
+
+}  // namespace
